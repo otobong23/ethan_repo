e121de623275caaf31d77f0dfa8656662f57a53d
miracle boniface <udohmiracle23@gmail.com>
2026-07-21 17:46:01 +0100

    admin dashboard updated

--- constants/profile.constant.ts
+++ constants/profile.constant.ts
@@ -3,7 +3,10 @@ export const USER_CONSTANT = {
    initials: 'RS',
    firstName: 'Ross',
    lastName: 'Campbell',
-   fullName: "Ross Campbell",
    email_address: "rosscampbell1090@gmail.com",
-   whatsapp_link: "https://wa.me/qr/OLAKNXGOQRO3B1"
+   whatsapp_link: "https://wa.me/qr/OLAKNXGOQRO3B1",
+
+   get fullName() {
+      return `${this.firstName} ${this.lastName}`;
+   }
 }
